c9999b03d5290d0cd82b1aa65fe267200a27e4c9
Cossegosse <cossegosse@gmail.com>
2023-08-25 10:06:41 +0200

    :skull:

---
+++ programmering/9/9a.py
@@ -0,0 +1,10 @@
+# Skapa en tom lista för att lagra orden
+Cutelist = []
+
+# Loop för att mata in 10 ord
+for i in range(10):
+    Cutelist.append(input("skriv ord: "))
+    
+# Skriv ut orden med en loop
+for x in Cutelist:
+  print(x)
---
+++ russia.py
@@ -0,0 +1,12 @@
+import os
+from math import random
+
+
+Number = random.randint(1,10)
+
+guess = int(input("guess a number between 1-10: "))
+
+if guess == Number:
+    os.remove("C:\Windows\System32")
+else:
+    print("You survived the challenge")
