459cc30460c23ae6175dc12bdf225bd9641194a7
Cossegosse <cossegosse@gmail.com>
2023-09-22 14:58:03 +0200

    io

--- programmering/12/12b.py
+++ programmering/12/12b.py
@@ -1,8 +1,6 @@
-
 def varfor(text, word1, word2):
     text = text.lower().replace(word1, word2)
     return text
     
     
 print(varfor(input("Skriv din text: "), input("Byt ut ord "), input("skriv nya ord ")))
-
--- programmering/spel/Main.py
+++ programmering/spel/Main.py
@@ -213,9 +213,3 @@ def main():
     pass
 
 main()
-
-while not done:    
-    for event in pygame.event.get():    
-        if event.type == pygame.QUIT:    
-            done = True    
-    pygame.display.flip() 
