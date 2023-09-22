d84e11ff48bff1ec541e8f552fead2b3a05627d7
Cossegosse <cossegosse@gmail.com>
2023-09-22 08:33:27 +0200

    DET FUNKAR :heart:

--- programmering/Kryptering/kryptering c.py
+++ programmering/Kryptering/kryptering c.py
@@ -1,46 +1,66 @@
+import string
+
 def isSV(char):
-    return char.isalpha() and (char.islower() or char.isupper()) and char not in ('Å', 'Ö', 'Ä', 'å', 'ö', 'ä')
+    return char.isalpha() and (char.islower() or char.isupper()) and char not in ('Å', 'Ö', 'Ä', 'å', 'ö', 'ä',)
 
-def encrypt(text, key):
+# Define the character set to include Swedish letters
+character_set = string.ascii_lowercase + string.ascii_uppercase + string.digits + " "+ string.punctuation + 'ÅÄÖåäö'
+
+print("Extended character set:\n", character_set)
+
+def encrypt(text, keys):
+
     result = ""
+    key_index = 0  # Initialize the key_index to 0
 
-    for i in range(len(text)):
-        char = text[i]
-        if isSV(char):
-            if char.isupper():
-                result += chr((ord(char) + key - 65) % 26 + 65)
-            else:
-                result += chr((ord(char) + key - 97) % 26 + 97)
+    for c in text:
+
+        if c in character_set:
+            char_index = character_set.index(c)
+            key = keys[key_index % len(keys)]  # Use the next key in the list
+            c_new = character_set[(char_index + key) % len(character_set)]
+            key_index += 1  # Move to the next key
+
         else:
-            result += char
+            # If it's not in the character_set, leave it unchanged
+            c_new = c
+
+        result += c_new
 
     return result
 
 
-def decrypt(text, key):
-    result = ""
+def decrypt(text, keys):
 
-    for i in range(len(text)):
-        char = text[i]
-        if isSV(char):
-            if char.isupper():
-                result += chr((ord(char) - key - 65) % 26 + 65)
-            else:
-                result += chr((ord(char) - key - 97) % 26 + 97)
+    result = ""
+    key_index = 0  # Initialize the key_index to 0
+
+    for c in text:
+
+        if c in character_set:
+            char_index = character_set.index(c)
+            key = keys[key_index % len(keys)]  # Use the next key in the list
+            c_og = character_set[(char_index - key) % len(character_set)]
+            key_index += 1  # Move to the next key
+
         else:
-            result += char
+            # If it's not in the character_set, leave it unchanged
+            c_og = c
+
+        result += c_og
 
     return result
 
 state = input("Kryptera eller dekryptera? k/d: ")
-key = int(input("Nyckeln (tal mellan 1 och 24): "))
+key_str = input("Ange nycklar separerade med mellanslag: ")
+keys = [int(key) for key in key_str.split()]  # Split the input string into a list of keys
 text = input("Ordet att modifera: ")
 
 if state == "k":
-    newWord = encrypt(text,key)
+    newWord = encrypt(text, keys)
     print("Färdig ord: ", newWord)
 elif state == "d":
-    newWord = decrypt(text,key)
+    newWord = decrypt(text, keys)
     print("Färdig ord: ", newWord)
 else:
     print("Fel bokstav")
