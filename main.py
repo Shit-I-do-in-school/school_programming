d828114390c083d0120548e42901a89495b04c98
David Adomonis <22naadom@mediagymnasiet.se>
2023-12-06 11:58:39 +0100

    fdgrdrcdtcyrv8gu

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-
-print(chr(10000))
+import keyboard
+import time
+time.sleep(3)
+for i in range(10):
+    for i in range(100):
+        keyboard.write("ퟶퟷퟸퟹퟺퟻ")
+        time.sleep(0.01)
+    keyboard.press_and_release("enter")
